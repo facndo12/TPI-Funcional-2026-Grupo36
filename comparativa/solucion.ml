a08e5939c1304c2999289ba4d92c245b64ba07e0
ezeveloso2005 <ezeveloso2005@gmail.com>
2026-06-16 12:35:39 -0300

    Funcion Transicion en ocaml añadida

--- comparativa/solucion.ml
+++ comparativa/solucion.ml
@@ -1,3 +1,67 @@
+(* =======================================================
+   FUNCION: transicion_v2
+
+   NATURALEZA: Pura
+   (No produce efectos secundarios. Para las mismas
+   entradas siempre devuelve la misma salida.)
+
+   ESTRATEGIA: Pattern Matching
+   (Utiliza match ... with para evaluar los distintos
+   estados y destinos posibles del semáforo.)
+
+   IMPACTO: No destructiva
+   (No modifica estructuras existentes, solamente
+   construye y devuelve una nueva tupla con el estado
+   y la acción correspondiente.)
+
+   ======================================================= *)
+type estado =
+  | EnRojo
+  | EnVerde
+  | EnAmarillo
+  | IntermitenteRojoVerde
+  | IntermitenteVerdeAmarillo
+  | IntermitenteAmarilloRojo
+
+type destino =
+  | Rojo
+  | Verde
+  | Amarillo
+  | AIntermitenteRojoVerde
+  | AIntermitenteVerdeAmarillo
+  | AIntermitenteAmarilloRojo
+
+let transicion_v2 color_actual cambiar_a =
+  match (color_actual, cambiar_a) with
+  | (EnRojo, AIntermitenteRojoVerde) ->
+      (EnRojo, "cambiar-a-intermitente-rojo-verde")
+
+  | (IntermitenteRojoVerde, Verde) ->
+      (IntermitenteRojoVerde, "cambiar-a-verde")
+
+  | (EnVerde, AIntermitenteVerdeAmarillo) ->
+      (EnVerde, "cambiar-a-intermitente-verde-amarillo")
+
+  | (IntermitenteVerdeAmarillo, Amarillo) ->
+      (IntermitenteVerdeAmarillo, "cambiar-a-amarillo")
+
+  | (EnAmarillo, AIntermitenteAmarilloRojo) ->
+      (EnAmarillo, "cambiar-a-intermitente-amarillo-rojo")
+
+  | (IntermitenteAmarilloRojo, Rojo) ->
+      (IntermitenteAmarilloRojo, "cambiar-a-rojo")
+
+  | _ ->
+      (color_actual, "accion-por-defecto")
+
+let () =
+  let (_, accion1) =
+    transicion_v2 IntermitenteRojoVerde  Verde
+  in
+  print_endline accion1
+;;
+
+
 (* ========================================================================== *)
 (* FUNCIÓN: Timer                                                             *)
 (* NATURALEZA: Pura                                                           *)
